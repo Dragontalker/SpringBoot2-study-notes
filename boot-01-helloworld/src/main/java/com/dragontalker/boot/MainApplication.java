82792a8cbd787f67550a1369ad8c4788a3b154c9
Richard <richard.yang.tong@gmail.com>
2021-07-15 14:57:27 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/MainApplication.java
@@ -1,5 +1,6 @@
 package com.dragontalker.boot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -8,4 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
 }
