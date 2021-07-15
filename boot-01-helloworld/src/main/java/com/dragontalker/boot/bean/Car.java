d8b7922312001eb4ee4f85005f59b71271406dd2
Richard <richard.yang.tong@gmail.com>
2021-07-15 19:47:12 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Car.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Car.java
@@ -4,4 +4,20 @@ public class Car {
 
     private String brand;
     private Integer price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
 }
