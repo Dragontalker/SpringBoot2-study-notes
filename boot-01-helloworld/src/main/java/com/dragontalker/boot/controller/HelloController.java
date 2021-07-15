5692dfc7ffde1ae0aa93cafcef50baaceacf3fd9
Richard <richard.yang.tong@gmail.com>
2021-07-15 19:51:47 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/controller/HelloController.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/controller/HelloController.java
@@ -1,10 +1,20 @@
 package com.dragontalker.boot.controller;
 
+import com.dragontalker.boot.bean.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    Car car;
+
+    @RequestMapping("/car")
+    public Car car() {
+        return car;
+    }
 
     @RequestMapping("/hello")
     public String handle01() {
