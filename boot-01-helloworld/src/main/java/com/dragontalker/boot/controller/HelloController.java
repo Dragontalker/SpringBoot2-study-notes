61a623ac92f893d4ed36d56787f3b6646128fa6d
Richard <richard.yang.tong@gmail.com>
2021-07-15 14:59:01 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/controller/HelloController.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/controller/HelloController.java
@@ -2,10 +2,12 @@ package com.dragontalker.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String handle01() {
         return "Hello, Spring Boot 2!";
