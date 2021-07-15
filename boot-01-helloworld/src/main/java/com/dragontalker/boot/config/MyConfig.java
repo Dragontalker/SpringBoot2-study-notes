9c9778f43d130950da76f9f7d392586a37dfc454
Richard <richard.yang.tong@gmail.com>
2021-07-15 16:34:54 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
@@ -8,8 +8,9 @@ import org.springframework.context.annotation.Configuration;
 /**
  * 1. 配置类里面使用@Bean标注在方法上给容器注册组件, 默认也是单实例的
  * 2. 配置类本身也是组件
+ * 3. proxyBeanMethods: 代理bean的方法
  */
-@Configuration // 告诉SpringBoot这是一个配置类
+@Configuration(proxyBeanMethods = true) // 告诉SpringBoot这是一个配置类
 public class MyConfig {
 
     @Bean // 给容器中添加组件, 以方法名作为组件的id, 返回类型就是组件类型, 返回的值, 就是容器中保存的实例
