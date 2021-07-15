3429bd35a2e409865995ff796d180fb891126b9a
Richard <richard.yang.tong@gmail.com>
2021-07-15 16:20:07 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/User.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/User.java
@@ -28,4 +28,12 @@ public class User {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
