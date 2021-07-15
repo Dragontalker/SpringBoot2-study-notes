74b23edabe6f9fb8f8bf81bc8ac3475088328dd2
Richard <richard.yang.tong@gmail.com>
2021-07-15 19:49:48 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Car.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Car.java
@@ -3,6 +3,9 @@ package com.dragontalker.boot.bean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * 只有在容器中的组件, 才能拥有SpringBoot提供的强大功能
+ */
 @Component
 @ConfigurationProperties(prefix = "mycar")
 public class Car {
