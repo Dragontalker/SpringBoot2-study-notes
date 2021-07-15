eb586d919ce9fa37c833008c3f830183a730f940
Richard <richard.yang.tong@gmail.com>
2021-07-15 16:27:19 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
@@ -1,7 +1,14 @@
 package com.dragontalker.boot.config;
 
+import com.dragontalker.boot.bean.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // 告诉SpringBoot这是一个配置类
 public class MyConfig {
+
+    @Bean // 给容器中添加组件, 以方法名作为组件的id, 返回类型就是组件类型, 返回的值, 就是容器中保存的实例
+    public User user01() {
+        return new User("zhangsan", 18);
+    }
 }
