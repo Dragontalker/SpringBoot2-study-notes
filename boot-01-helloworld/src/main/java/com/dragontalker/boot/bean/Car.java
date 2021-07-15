7a282963ec43d44f28bc74e4fd74a76eeacfb90f
Richard <richard.yang.tong@gmail.com>
2021-07-15 19:49:23 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Car.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Car.java
@@ -1,8 +1,10 @@
 package com.dragontalker.boot.bean;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
-@ConfigurationProperties
+@Component
+@ConfigurationProperties(prefix = "mycar")
 public class Car {
 
     private String brand;
