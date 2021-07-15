c987d3655711af205c182e96d747ed50c2b4a2b6
Richard <richard.yang.tong@gmail.com>
2021-07-15 16:18:14 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Pet.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Pet.java
@@ -1,4 +1,14 @@
 package com.dragontalker.boot.bean;
 
 public class Pet {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
