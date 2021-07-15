8e1af813426a3e14fe5bb128f490ddc2b2a4de60
Richard <richard.yang.tong@gmail.com>
2021-07-15 16:38:35 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/MainApplication.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/MainApplication.java
@@ -29,9 +29,13 @@ public class MainApplication {
         Pet tom02 = run.getBean("tom", Pet.class);
         System.out.println(tom01 == tom02);
 
+        // 4. com.dragontalker.boot.config.MyConfig$$EnhancerBySpringCGLIB$$aab697e@27aae97b
         MyConfig bean = run.getBean(MyConfig.class);
         System.out.println(bean);
 
+        // 如果@Configuration(proxyBeanMethods = true)代理对象调用方法.
+        // SpringBoot总会检查这个组件是否在容器中有
+        // 保持组件单实例
         User user01 = run.getBean(User.class);
         User user02 = run.getBean(User.class);
         System.out.println(user01 == user02);
