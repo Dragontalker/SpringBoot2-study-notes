2ab429b82877cc75e36e672807cbf0cfd2fb196d
Richard <richard.yang.tong@gmail.com>
2021-07-15 19:38:30 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
@@ -21,12 +21,12 @@ import org.springframework.context.annotation.Import;
  */
 @Import({User.class, DBHelper.class})
 @Configuration(proxyBeanMethods = false) // 告诉SpringBoot这是一个配置类
+@ConditionalOnBean(name = "tom")
 public class MyConfig {
 
     /**
      * 外部无论对配置类中的这个组件注册方法调用多少次获取的都是之前注册容器的单实例对象
      */
-    @ConditionalOnBean(name = "tom")
     @Bean // 给容器中添加组件, 以方法名作为组件的id, 返回类型就是组件类型, 返回的值, 就是容器中保存的实例
     public User user01() {
         User zhangsan = new User("zhangsan", 18);
