52c59aab449cc7c996cd832850b5ee3803e0e580
Richard <richard.yang.tong@gmail.com>
2021-07-15 16:28:26 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Pet.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/Pet.java
@@ -7,6 +7,13 @@ public class Pet {
 
     private String name;
 
+    public Pet() {
+    }
+
+    public Pet(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
--- boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/config/MyConfig.java
@@ -1,5 +1,6 @@
 package com.dragontalker.boot.config;
 
+import com.dragontalker.boot.bean.Pet;
 import com.dragontalker.boot.bean.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class MyConfig {
     @Bean // 给容器中添加组件, 以方法名作为组件的id, 返回类型就是组件类型, 返回的值, 就是容器中保存的实例
     public User user01() {
         return new User("zhangsan", 18);
+    }
+
+    @Bean
+    public Pet tomcatPet() {
+        return new Pet("tomcat");
     }
 }
