4c0596b019d46e09c8babb9f89438b6ec88b5046
Richard <richard.yang.tong@gmail.com>
2021-07-16 14:50:47 -0400

    Added notes.

---
+++ boot-05-web-01/src/test/java/com/dragontalker/boot05web01/Boot05Web01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragontalker.boot05web01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05Web01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
