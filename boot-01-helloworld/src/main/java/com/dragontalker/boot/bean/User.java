02b562b358ebb8baae93ce428ad1f23d616a00e1
Richard <richard.yang.tong@gmail.com>
2021-07-16 10:13:03 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/User.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/bean/User.java
@@ -1,6 +1,8 @@
 package com.dragontalker.boot.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
@@ -8,6 +10,8 @@ import lombok.ToString;
  */
 @ToString
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private String name;
