aa1dac32245b51c568883231dd38ce865dc61e8f
Richard <richard.yang.tong@gmail.com>
2021-07-16 11:29:21 -0400

    Added notes.

--- boot-01-helloworld-02/src/main/java/com/dragontalker/boot01helloworld02/bean/Person.java
+++ boot-01-helloworld-02/src/main/java/com/dragontalker/boot01helloworld02/bean/Person.java
@@ -1,4 +1,20 @@
 package com.dragontalker.boot01helloworld02.bean;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Person {
+
+    private String userName;
+    private Boolean boss;
+    private Date birth;
+    private Integer age;
+    private Pet pet;
+    private String[] interests;
+    private List<String> animal;
+    private Map<String, Object> score;
+    private Set<Double> salarys;
+    private Map<String, List<Pet>> allPets;
 }
