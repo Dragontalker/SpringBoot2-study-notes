93641d40401463565d2103633951f3f1ab885aa0
Richard <richard.yang.tong@gmail.com>
2021-07-16 10:15:02 -0400

    Added notes.

--- boot-01-helloworld/src/main/java/com/dragontalker/boot/controller/HelloController.java
+++ boot-01-helloworld/src/main/java/com/dragontalker/boot/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.dragontalker.boot.controller;
 
 import com.dragontalker.boot.bean.Car;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -20,6 +22,7 @@ public class HelloController {
     @RequestMapping("/hello")
     public String handle01(@RequestParam("name") String name) {
 
+        log.info("请求进来啦...");
         return "Hello, Spring Boot 2! " + name;
     }
 }
