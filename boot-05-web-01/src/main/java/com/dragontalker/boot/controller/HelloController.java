37b62a06d2867eaff49ed8e23e7b4b7ae3abc726
Richard <richard.yang.tong@gmail.com>
2021-07-16 14:57:59 -0400

    Added notes.

--- boot-05-web-01/src/main/java/com/dragontalker/boot/controller/HelloController.java
+++ boot-05-web-01/src/main/java/com/dragontalker/boot/controller/HelloController.java
@@ -1,7 +1,13 @@
 package com.dragontalker.boot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @RequestMapping("/bug.jpg")
+    public String hello() {
+        return "aaaa";
+    }
 }
