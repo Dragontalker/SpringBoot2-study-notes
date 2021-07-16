c4cd3ca44d204377b7eb1810fce5229f01807f99
Richard <richard.yang.tong@gmail.com>
2021-07-16 11:45:42 -0400

    Added notes.

--- boot-01-helloworld-02/src/main/java/com/dragontalker/boot/controller/HelloController.java
+++ boot-01-helloworld-02/src/main/java/com/dragontalker/boot/controller/HelloController.java
@@ -1,7 +1,18 @@
 package com.dragontalker.boot.controller;
 
+import com.dragontalker.boot.bean.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    Person person;
+
+    @RequestMapping("/person")
+    public Person person() {
+        return person;
+    }
 }
