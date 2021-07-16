3f057356097c99984fe551b8ca3b23d8a055e534
Richard <richard.yang.tong@gmail.com>
2021-07-16 16:38:40 -0400

    Added notes.

--- boot-05-web-01/src/main/java/com/dragontalker/boot/controller/HelloController.java
+++ boot-05-web-01/src/main/java/com/dragontalker/boot/controller/HelloController.java
@@ -1,8 +1,6 @@
 package com.dragontalker.boot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -12,22 +10,22 @@ public class HelloController {
         return "aaaa";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping("/user")
     public String getUser() {
        return "GET-张三";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @PostMapping("/user")
     public String postUser() {
         return "POST-张三";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    @PutMapping("/user")
     public String putUser() {
         return "PUT-张三";
     }
 
-    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    @DeleteMapping("/user")
     public String deleteUser() {
         return "DELETE-张三";
     }
