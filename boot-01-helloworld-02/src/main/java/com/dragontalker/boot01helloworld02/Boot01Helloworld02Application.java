5fb816ba41930081126ad6cd9e7803a03e37f9b3
Richard <richard.yang.tong@gmail.com>
2021-07-16 11:25:43 -0400

    Added notes.

---
+++ boot-01-helloworld-02/src/main/java/com/dragontalker/boot01helloworld02/Boot01Helloworld02Application.java
@@ -0,0 +1,13 @@
+package com.dragontalker.boot01helloworld02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot01Helloworld02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot01Helloworld02Application.class, args);
+	}
+
+}
---
+++ boot-01-helloworld-02/src/test/java/com/dragontalker/boot01helloworld02/Boot01Helloworld02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragontalker.boot01helloworld02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot01Helloworld02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
