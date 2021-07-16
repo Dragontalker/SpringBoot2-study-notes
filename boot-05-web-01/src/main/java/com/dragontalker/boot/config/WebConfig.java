5eb095aaf1e6ef3de932f5dd9c00bb55912cc1d8
Richard <richard.yang.tong@gmail.com>
2021-07-16 16:41:00 -0400

    Added notes.

--- boot-05-web-01/src/main/java/com/dragontalker/boot/config/WebConfig.java
+++ boot-05-web-01/src/main/java/com/dragontalker/boot/config/WebConfig.java
@@ -1,8 +1,15 @@
 package com.dragontalker.boot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration(proxyBeanMethods = false)
 public class WebConfig {
 
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+        return methodFilter;
+    }
 }
